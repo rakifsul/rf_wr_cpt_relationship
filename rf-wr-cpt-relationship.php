9a63afaf06d90aea90cc6a6556e36ac4633ba6c5
rakifsul <allwebs200@gmail.com>
2024-10-03 06:37:23 +0700

    update

--- rf-wr-cpt-relationship.php
+++ rf-wr-cpt-relationship.php
@@ -11,7 +11,7 @@ Text Domain: rf-wr-plugin
 */
 
 function rel111_create_custom_post_types() {
-    // Register Custom Post Type: Books
+    // register Custom Post Type: Books
     register_post_type('books', array(
         'labels'      => array(
             'name'          => __('Books'),
@@ -23,7 +23,7 @@ function rel111_create_custom_post_types() {
         'supports'    => array('title', 'editor', 'thumbnail'),
     ));
 
-    // Register Custom Post Type: Authors
+    // register Custom Post Type: Authors
     register_post_type('authors', array(
         'labels'      => array(
             'name'          => __('Authors'),
@@ -37,31 +37,31 @@ function rel111_create_custom_post_types() {
 }
 add_action('init', 'rel111_create_custom_post_types');
 
-// Add meta box for selecting related authors
+// tambahkan meta box
 function rel111_add_author_meta_box() {
     add_meta_box(
-        'related_authors',           // Unique ID
-        'Related Authors',           // Box title
-        'rel111_render_author_meta_box',    // Content callback, must be of type callable
-        'books',                     // Post type
-        'side',                      // Context (where the meta box will appear)
-        'default'                    // Priority
+        'related_authors',
+        'Related Authors',
+        'rel111_render_author_meta_box',
+        'books',
+        'side',
+        'default'
     );
 }
 add_action('add_meta_boxes', 'rel111_add_author_meta_box');
 
-// Render the meta box content
+// render konten meta box
 function rel111_render_author_meta_box($post) {
-    // Get the existing value (related authors) if any
+    // dapatkan value (related authors) yang sudah ada jika ada
     $selected_authors = get_post_meta($post->ID, 'related_authors', true);
 
-    // Get all authors to populate the select dropdown
+    // dapatkan semua authors untuk mengisi dropdown
     $authors = get_posts(array(
         'post_type'   => 'authors',
         'numberposts' => -1
     ));
 
-    // Render a select dropdown with all authors
+    // render dropdown
     echo '<select name="related_authors[]" multiple="multiple" style="width: 100%; height: 150px;">';
     foreach ($authors as $author) {
         $selected = (is_array($selected_authors) && in_array($author->ID, $selected_authors)) ? 'selected="selected"' : '';
@@ -70,19 +70,19 @@ function rel111_render_author_meta_box($post) {
     echo '</select>';
 }
 
-// Save selected authors when the post is saved
+// save author terpilih
 function rel111_save_related_authors($post_id) {
-    // Check if the current user has permission to edit the post
+    // jika user bisa edit_post
     if (!current_user_can('edit_post', $post_id)) {
         return;
     }
 
-    // Sanitize and save the selected authors
+    // sanitize dan save selected authors
     if (isset($_POST['related_authors'])) {
         $authors = array_map('intval', $_POST['related_authors']);
         update_post_meta($post_id, 'related_authors', $authors);
     } else {
-        // If no authors were selected, delete the meta key
+        // jika tidak ada author selected, delete meta key
         delete_post_meta($post_id, 'related_authors');
     }
 }
